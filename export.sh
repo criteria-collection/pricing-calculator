aa39ff288e3fdea4fad75263543086a1c76e81f0
Adam Clarke <adamc@strategicdata.com.au>
2018-08-23 18:24:57 +1000

    cookes from file, export to test dir

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -X GET http://45.76.127.70/api/wp-admin/admin-ajax.php?action=collection_dump \
-  -b cookies \
-  > export-$(date +%F).json
+  --cookie cookies \
+  > test-data/export-$(date +%F).json
